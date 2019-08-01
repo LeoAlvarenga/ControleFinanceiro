53ccb7a60b4f117596933a02a7dd7536edd5ab8b
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2019-08-01 16:17:23 -0300

    modificações na interface

---
+++ lib/models/operacao.dart
@@ -0,0 +1,11 @@
+
+class Operacao {
+
+    Operacao(tipo, valor, data, {observacao});
+
+    int id;
+    String tipo;
+    double valor;
+    DateTime data;
+    String observacao;
+}
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:date_format/date_format.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +7,34 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+
+  List<int> operacoes = List();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,21 +43,64 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.deepPurple,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Container(
-          child: ListView.builder(
-            itemBuilder: (context, index){
-              return _movimentacao(context, index);
-              },
-          ),
-        ),
+      body:
+        Column(
+          children: <Widget>[
+            Card(
+              child: Row(
+                children: <Widget>[
+                      Text("Total Gasto: xxxxx",
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
+                      textAlign: TextAlign.center,
+                       )
+                ],
+              ),
+             color: Colors.deepPurple,
+            ),
+            Expanded(
+              child: Padding(
+              padding: EdgeInsets.all(10.0),
+                child: ListView.builder(
+                  itemBuilder: (context, index){
+                    return _movimentacao(context, index);
+                    },
+                  itemCount: 3,
+                ),
+              ),
+              ),
+
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.deepPurple,
           child: Icon(Icons.add),
-          onPressed: (){},
+          onPressed: (){print(operacoes.length);},
         ),
+        bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_membership),
+            title: Text('Crédito'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.monetization_on),
+            title: Text('Débito'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            title: Text('Saque'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.deepPurple,
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.deepPurpleAccent[100],
+        onTap: _onItemTapped,
+      ),
     );
   }
 
@@ -39,9 +111,19 @@ class _HomeState extends State<Home> {
           padding: EdgeInsets.all(10.0),
           child: Row(
             children: <Widget>[
-              Text("Valor"),
-              Text("Tipo do pagamento"),
-              Text(DateTime.now().toString()),
+              Column(
+                children: <Widget>[
+                  Icon(Icons.attach_money)
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                Text("Tipo do pagamento",textAlign: TextAlign.start, style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold)),
+                Text("Valor", textAlign: TextAlign.start, style: TextStyle(fontSize: 15.0,)),
+                Text(formatDate(DateTime.now(),[dd, '/', mm, '/', yyyy, ' ', HH,':', nn]).toString(), textAlign: TextAlign.start,),
+                ],
+              )
+
             ],
           ),
         ),
